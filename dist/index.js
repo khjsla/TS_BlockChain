39b767980b8a3d71dec01208632395544863c417
khjsla <rkdguswlsla@naver.com>
2021-05-16 19:45:29 +0900

    [-ing 2 - til 10]make block chain

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+//RUN WITH npm start.
+const CyptoJs = require("crypto-js");
 class Block {
+    //IT CAN BE WITH [ STATIC ]
+    //actually, it have to exist in outside
     constructor(index, hash, previousHash, data, timestamp) {
         this.index = index;
         this.hash = hash;
@@ -9,6 +13,8 @@ class Block {
         this.timestamp = timestamp;
     }
 }
+//Block 을 생성하지 않아도, 사용가능한 method 를 만들고 싶으면 ,,
+Block.calculateBlockHash = (index, previousHash, timestamp, data) => CyptoJs.SHA256(index + previousHash + timestamp + data).toString();
 const genesisBlock = new Block(0, "202020202020202", "", "Hello", 123456);
 let blockchain = [genesisBlock];
 //[Block] = this mean == array of Block .
@@ -17,4 +23,17 @@ let blockchain = [genesisBlock];
 // ㄴ It Can not be work because of String is NOT a BLOCK
 // and this is the smart of the TS
 console.log('blockchain: ', blockchain);
+const getBlockchain = () => blockchain; //[Block] == Block[]
+//this is return blockchain.
+const getLatestBlock = () => blockchain[blockchain.length - 1]; //this is how to get latest
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data) => {
+    const previousBlock = getLatestBlock(); //get prev block
+    const newIndex = previousBlock.index + 1; //new index. 
+    const newTimestamp = getNewTimeStamp();
+    const newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data); //tis need index
+    const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp); //this is NEW BLOCK.
+    return newBlock;
+};
+console.log(createNewBlock("helo"), createNewBlock("byby"));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 //RUN WITH npm start.
+import * as CyptoJs from 'crypto-js';
+
 
 class Block {
      public index: number;
@@ -7,13 +9,18 @@ class Block {
      public data: string;
      public timestamp: number;
 
+     //Block 을 생성하지 않아도, 사용가능한 method 를 만들고 싶으면 ,,
+     static calculateBlockHash = (index: number, previousHash: string, timestamp: number, data: string): string => CyptoJs.SHA256(index + previousHash + timestamp + data).toString();
+     //IT CAN BE WITH [ STATIC ]
+     //actually, it have to exist in outside
+
      constructor(
           index: number,
           hash: string,
           previousHash: string,
           data: string,
           timestamp: number,
-     ){
+     ) {
           this.index = index;
           this.hash = hash;
           this.previousHash = previousHash;
@@ -22,9 +29,9 @@ class Block {
      }
 }
 
-const genesisBlock:Block = new Block(0,"202020202020202","","Hello", 123456);
+const genesisBlock: Block = new Block(0, "202020202020202", "", "Hello", 123456);
 
-let blockchain: [Block] = [genesisBlock]; 
+let blockchain: [Block] = [genesisBlock];
 //[Block] = this mean == array of Block .
 //and can make array *ONLY* with Block. 
 
@@ -34,6 +41,23 @@ let blockchain: [Block] = [genesisBlock];
 
 console.log('blockchain: ', blockchain);
 
+const getBlockchain = (): Block[] => blockchain; //[Block] == Block[]
+//this is return blockchain.
 
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1]; //this is how to get latest
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => { //have to have data argu , and this is return Block
+     const previousBlock: Block = getLatestBlock(); //get prev block
+     const newIndex: number = previousBlock.index + 1; //new index. 
+     const newTimestamp: number = getNewTimeStamp();
+     const newHash: string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data); //tis need index
+     const newBlock: Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp); //this is NEW BLOCK.
+     
+     return newBlock;
+};
+
+console.log(createNewBlock("helo"),createNewBlock("byby"))
 
 export { };

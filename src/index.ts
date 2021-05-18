3742d28754fd405462978779190c051960af2a4a
khjsla <rkdguswlsla@naver.com>
2021-05-18 14:13:22 +0900

    ts basic with create blockchain

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,24 @@ import * as CyptoJs from 'crypto-js';
 
 
 class Block {
+     //Block 을 생성하지 않아도, 사용가능한 method 를 만들고 싶으면 ,,
+     static calculateBlockHash = (index: number, previousHash: string, timestamp: number, data: string): string => CyptoJs.SHA256(index + previousHash + timestamp + data).toString();
+     //IT CAN BE WITH [ STATIC ]
+     //actually, it have to exist in outside
+
+     static validateSturcture = (aBlock: Block): boolean => 
+     typeof aBlock.index === "number" && 
+     typeof aBlock.hash === "string" && 
+     typeof aBlock.previousHash === "string" && 
+     typeof aBlock.timestamp === "number" && 
+     typeof aBlock.data == "string"; //; must
+
+     //after static is better !
      public index: number;
      public hash: string;
      public previousHash: string;
      public data: string;
      public timestamp: number;
-
-     //Block 을 생성하지 않아도, 사용가능한 method 를 만들고 싶으면 ,,
-     static calculateBlockHash = (index: number, previousHash: string, timestamp: number, data: string): string => CyptoJs.SHA256(index + previousHash + timestamp + data).toString();
-     //IT CAN BE WITH [ STATIC ]
-     //actually, it have to exist in outside
 
      constructor(
           index: number,
@@ -53,11 +61,51 @@ const createNewBlock = (data: string): Block => { //have to have data argu , and
      const newIndex: number = previousBlock.index + 1; //new index. 
      const newTimestamp: number = getNewTimeStamp();
      const newHash: string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data); //tis need index
-     const newBlock: Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp); //this is NEW BLOCK.
-     
+     const newBlock: Block = new Block(
+          newIndex, 
+          newHash, 
+          previousBlock.hash, 
+          data, 
+          newTimestamp
+     ); //this is NEW BLOCK.
+
+     //block 이 추가되면, 블록체인에 추가 할거임
+     addBlock(newBlock); **
+
      return newBlock;
 };
 
-console.log(createNewBlock("helo"),createNewBlock("byby"))
+const getHashforBlock = (aBlock: Block):string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data)
+
+//IS structure valid?
+// - validating structure
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+     if (!Block.validateSturcture(candidateBlock)){
+          return false;
+     } else if(previousBlock.index +1 !== candidateBlock.index){
+          return false
+     } else if(previousBlock.hash !== candidateBlock.previousHash){
+          return false;
+     } else if(getHashforBlock(candidateBlock) !== candidateBlock.hash){ 
+          return false;
+     } else{
+          return true;
+     } //get the hash from block
+}; //구조검증 //hash검증.
+
+//블록에 블록체인 추가하면 됨
+const addBlock = (candidateBlock: Block): void =>{
+     if(isBlockValid(candidateBlock, getLatestBlock())){
+          blockchain.push(candidateBlock);
+     }
+}
+
+createNewBlock("seconde block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+
+console.log(blockchain);
+
+console.log(createNewBlock("helo"), createNewBlock("byby"))
 
 export { };
